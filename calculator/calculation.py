ce7ad91bc79c675cdf1ae274ec617c1651e7ef3c
db-48 <db48@njit.edu>
2024-06-14 19:20:55 -0400

    added comments to calculation file

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -2,7 +2,8 @@ class Calculation:
     def __init__(self, a, b, operation):
         self.a = a
         self.b = b
-        self.operation = operation  
+        self.operation = operation #Stores the operation function 
 
     def get_result(self):
+        #Call the stored operation
         return self.operation(self.a, self.b)
