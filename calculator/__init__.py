2fa31a308ee8c9a9db20e243b06eac5c4a0c6646
db-48 <db48@njit.edu>
2024-06-14 19:58:29 -0400

    updated init

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -4,18 +4,20 @@ from calculator.operations import add, subtract, multiply, divide
 class Calculator:
     @staticmethod
     def add(a,b):
-        calculation = Calculation(a, b, add)  
+        calculation = Calculation(a, b, add)  # Pass the add function from calculator.operations
         return calculation.get_result()
     @staticmethod
     def subtract(a,b):
-        calculation = Calculation(a, b, subtract)  
+        calculation = Calculation(a, b, subtract)  # Pass the add function from calculator.operations
         return calculation.get_result()
     @staticmethod
     def multiply (a,b):
-        calculation = Calculation(a, b, multiply)  
+        calculation = Calculation(a, b, multiply)  # Pass the add function from calculator.operations
         return calculation.get_result()
     @staticmethod
     def divide(a,b):
-        calculation = Calculation(a, b, divide)  
+        calculation = Calculation(a, b, divide)  # Pass the add function from calculator.operations
         return calculation.get_result()
+
+
 
